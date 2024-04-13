a72ebe792dd32f8505055fb9e4a35e0f1e04e118
go2023 <tetyanago2018@gmail.com>
2024-04-13 15:00:26 -0700

    handleDeleteBlog function was added to home page

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,30 +1,14 @@
 import {useContext, useEffect} from 'react';
 import { GlobalContext } from "../../context";
 import axios from "axios";
+import classes from "./styles.module.css";
+import { FaTrash, FaEdit } from "react-icons/fa";
 
 const Home = () => {
     const {blogList,
         setBlogList,
         pending,
         setPending} = useContext(GlobalContext);
-
-    // const fetchListOfBlogs = async () => {
-    //     setPending(true);
-    //     try {
-    //         const response = await axios.get("/api/blogs");
-    //         const result = await response.data;
-    //         console.log(result);
-    //         if (result && result.blogList && result.blogList.length) {
-    //             setBlogList(result.blogList);
-    //             setPending(false);
-    //         } else {
-    //             setPending(false);
-    //             setBlogList([]);
-    //         }
-    //     } catch (error) {
-    //         console.error("Error fetching blogs:", error);
-    //     }
-    // };
 
     const fetchListOfBlogs = async () => {
         setPending(true);
@@ -35,7 +19,9 @@ const Home = () => {
             // Check if response has a blogList property
             if (result && result.length) {
                 setBlogList(result); // Set blogList directly
+                setPending(false);
             } else {
+                setPending(false);
                 setBlogList([]); // Set empty array if no blogs found
             }
         } catch (error) {
@@ -45,26 +31,46 @@ const Home = () => {
         }
     };
 
+    const handleDeleteBlog = async (getCurrentId) => {
+        try {
+            const response = await axios.delete(`/api/blogs/delete/${getCurrentId}`);
+            const result = await response.data;
+
+            if(result?.message) {
+                fetchListOfBlogs();
+            }
+        } catch (error) {
+            console.error("Error deleting blog:", error);
+        }
+
+    }
+
 
     useEffect(() => {
         fetchListOfBlogs();
     }, []);
 
     return (
-        <div>
+        <div className={classes.wrapper}>
             <h1>Blog List</h1>
             {
                 pending ? (
                     <h1>Loading...</h1>
                 ) : (
-                    <div>
+                    <div className={classes.blogList}>
                         {
+                            blogList && blogList.length ? (
                             blogList.map((blogItem) => (
                                 <div key={blogItem._id}>
                                     <p>{blogItem.title}</p>
                                     <p>{blogItem.description}</p>
+                                    <FaEdit size={30} />
+                                    <FaTrash onClick={() => handleDeleteBlog(blogItem._id)} size={30} />
                                 </div>
                             ))
+                            ) : (
+                                <h1>No blogs found</h1>
+                            )
                         }
                     </div>
                 )
